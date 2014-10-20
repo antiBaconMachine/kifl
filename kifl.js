d0cfdf87c134049a336e2a121abbcccdcc778634
Ollie Edwards <olliee@mobbu.com>
2014-10-20 17:01:32 +0100

    embryonic drag dupport

--- kifl.js
+++ kifl.js
@@ -1,23 +1,51 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    // counter starts at 0
+    Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+//    Template.hello.helpers({
+//        counter: function () {
+//            return Session.get("counter");
+//        }
+//    });
+//
+//    Template.hello.events({
+//        'click button': function () {
+//            // increment the counter when button is clicked
+//            Session.set("counter", Session.get("counter") + 1);
+//        }
+//    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+    Template.column.helpers({
+        cards: function() {
+            return [{
+                title: 'dummy'
+            }]
+        }
+    });
+
+    var filterEvent = function(selector, callback) {
+        return function(e) {
+            if (e.target.matches(selector)) {
+                callback(e);
+                e.stopPropagation();
+            }
+        }
     }
-  });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var grid = document.querySelector('.grid');
+        grid.addEventListener('dragstart', filterEvent('.card', function(e) {
+            e.target.style.opacity = '0.4';
+        }), false);
+        grid.addEventListener('dragend', filterEvent('.card', function(e) {
+            e.target.style.opacity = '1';
+        }), false);
+    });
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
